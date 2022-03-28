2621109164c122d8fa1b6660e4a85c8f20c4f369
israaHamdi <israaaburayya2012@gmail.com>
2022-03-28 21:07:55 +0200

    edit build.sql file

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -1,10 +1,10 @@
 BEGIN;
-drop table if exists users, posts, comments CASCADE;;
+drop table if exists users, posts, comments CASCADE;
 create table users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE posts (
@@ -15,13 +15,14 @@ CREATE TABLE posts (
   user_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   description TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   post_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
-  FOREIGN KEY (post_id) REFERENCES posts (id)
+  FOREIGN KEY (post_id) REFERENCES posts (id),
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 COMMIT;
